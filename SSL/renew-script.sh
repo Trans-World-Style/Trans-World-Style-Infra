a408ea62be23da365d6e9b5e7b7f36e5aea8dac5
강두원 <kangdoowon@gangduwon-ui-MacBookPro.local>
2023-07-15 13:30:39 +0900

    add SSL etc container
    add SSL etc container
    
    add SSL etc container
    
    add SSL etc container
    
    add SSL etc container
    
    add SSL etc container
    
    add SSL etc container
    
    add SSL etc container
    
    add SSL etc container
    
    add SSL etc container
    
    add SSL etc container
    
    add SSL etc container
    
    add SSL etc container
    
    test
    
    test
    
    test
    
    test
    
    test
    
    test
    
    test
    
    add spring ssl
    
    add spring ssl
    
    add spring ssl
    
    add ssl volume
    
    add ssl volume
    
    add ssl volume
    
    add ssl volume

---
+++ SSL/renew-script.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# The environment variables DOMAIN and EMAIL should be set.
+if [ -z "$DOMAIN" ] || [ -z "$EMAIL" ]; then
+  echo "The DOMAIN and EMAIL environment variables must be set."
+  exit 1
+fi
+
+# If the certificate is not present, request a new one.
+# If the certificate is present but near expiry, request a new one.
+# Otherwise, sleep.
+while :; do
+  if ! certbot certificates | grep -q "$DOMAIN"; then
+    certbot certonly --standalone -d "$DOMAIN" -m "$EMAIL" --agree-tos --no-eff-email
+    openssl pkcs12 -export -in ./live/$DOMAIN/fullchain.pem -inkey ./live/$DOMAIN/privkey.pem -out ./live/$DOMAIN/keystore.p12 -name ttp -CAfile ./live/$DOMAIN/chain.pem -caname root -passout pass:0000
+  elif openssl x509 -checkend 2592000 -noout -in "/etc/letsencrypt/live/$DOMAIN/fullchain.pem"; then
+    echo "The certificate is up to date, sleeping..."
+    sleep 12h
+    continue
+  else
+    certbot renew
+    openssl pkcs12 -export -in ./live/$DOMAIN/fullchain.pem -inkey ./live/$DOMAIN/privkey.pem -out ./live/$DOMAIN/keystore.p12 -name ttp -CAfile ./live/$DOMAIN/chain.pem -caname root -passout pass:0000
+  fi
+  sleep 12h
+done
+
+# openssl pkcs12 -export -in fullchain.pem -inkey privkey.pem -out keystore.p12 -name ttp -CAfile chain.pem -caname root -passout pass:0000
+    # openssl pkcs12 -export -in ./live/$DOMAIN/fullchain.pem -inkey ./live/$DOMAIN/privkey.pem -out ./live/$DOMAIN/keystore.p12 -name ttp -CAfile ./live/$DOMAIN/chain.pem -caname root -passin pass:$PW -passout pass:$PW
