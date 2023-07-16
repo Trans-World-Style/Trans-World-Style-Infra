fbc538448855c5c750f8351a3084f7638d5a894d
강두원 <kangdoowon@gangduwon-ui-MacBookPro.local>
2023-07-16 21:07:44 +0900

    test

--- test/test.sh
+++ test/test.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 mkdir workspace2
-cp -rL /workspace/live/tw-style-duckdns.org /workspace2
-chmod -R 777 /workspace2/tw-style-duckdns.org
-cd /workspace2/tw-style-duckdns.org
+cp -rL /workspace/live/tw-style.duckdns.org /workspace2
+chmod -R 777 /workspace2/tw-style.duckdns.org
+cd /workspace2/tw-style.duckdns.org
 openssl pkcs12 -export -in fullchain.pem -inkey privkey.pem -out keystore.p12 -name ttp -CAfile chain.pem -caname root -passin pass:0000 -passout pass:0000
