e2ade734cb3390abbe7446fbddc9cef50dccc689
DW-K <pch145@naver.com>
2023-09-25 00:26:59 +0900

    create: shared libraries code

---
+++ k8s/jenkins/sharedLibraries/vars/getKanikoAgent.groovy
@@ -0,0 +1,34 @@
+def call() {
+    return {
+        kubernetes {
+            cloud "kubernetes-docker-job"
+            yaml '''
+                apiVersion: v1
+                kind: Pod
+                metadata:
+                  labels:
+                    role: kaniko
+                spec:
+                  containers:
+                  - name: kaniko
+                    image: gcr.io/kaniko-project/executor:debug
+                    command:
+                    - /busybox/cat
+                    imagePullPolicy: Always
+                    tty: true
+                    volumeMounts:
+                    - name: jenkins-docker-cfg
+                      mountPath: /kaniko/.docker
+                  volumes:
+                  - name: jenkins-docker-cfg
+                    projected:
+                      sources:
+                      - secret:
+                          name: dockerhub-secret
+                          items:
+                            - key: .dockerconfigjson
+                              path: config.json
+                '''
+        }
+    }
+}
