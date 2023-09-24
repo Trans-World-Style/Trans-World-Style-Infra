6d978ac05101f154ad3454437422a2c57483536c
DW-K <pch145@naver.com>
2023-09-25 01:38:33 +0900

    create: shared libraries code

--- k8s/jenkins/sharedLibraries/vars/buildAndPush.groovy
+++ k8s/jenkins/sharedLibraries/vars/buildAndPush.groovy
@@ -2,8 +2,11 @@ def call(String dockerUsername, String imageName, String dockerTag) {
     container('kaniko') {
         script {
             def imageFullName = "${dockerUsername}/${imageName}:${dockerTag}"
+            // sh """
+            // /kaniko/executor --context `pwd` --verbosity debug --destination ${imageFullName} --cache
+            // """
             sh """
-            /kaniko/executor --context `pwd` --verbosity debug --destination ${imageFullName} --cache
+            /kaniko/executor --context `pwd` --destination ${imageFullName} --cache
             """
         }
     }
