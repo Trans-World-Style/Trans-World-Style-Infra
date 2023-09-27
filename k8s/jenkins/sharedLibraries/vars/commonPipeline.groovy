0b81ccd847f1856ab888a4e736aa8db408652e90
DW-K <pch145@naver.com>
2023-09-27 19:13:01 +0900

    feat: argocd

--- k8s/jenkins/sharedLibraries/vars/commonPipeline.groovy
+++ k8s/jenkins/sharedLibraries/vars/commonPipeline.groovy
@@ -88,7 +88,6 @@ def call(Closure body) {
                                 sh 'git clone https://$GITHUB_APP:$GITHUB_ACCESS_TOKEN@github.com/$MANIFEST_REPO'
                                 dir("${MANIFEST_REPO.split('/')[1].replace('.git', '')}") {
                                     def branch_name = "${env.GIT_BRANCH}"
-                                    sh "echo ${branch_name}"
                                     sh """
                                         sed -i 's|${DOCKERHUB_USERNAME}/${IMAGE_NAME}:.*|${DOCKERHUB_USERNAME}/${IMAGE_NAME}:${env.DOCKER_TAG}|' ${MANIFEST_DIR}/${MANIFEST_FILE}
                                         git config user.name "${env.GIT_AUTHOR_NAME}"
@@ -99,49 +98,7 @@ def call(Closure body) {
                                     sh 'git push https://$GITHUB_APP:$GITHUB_ACCESS_TOKEN@github.com/$MANIFEST_REPO $branch_name'
                                 }
                             }
-                            sh "pwd"
-                            sh "ls"
-                            sh "echo ${MANIFEST_DIR}"
-                            sh "echo ${AGENT_WORKDIR}"
-                            // if (!fileExists(MANIFEST_DIR)) {
-                            //     sh "git clone https://${GITHUB_APP}:${GITHUB_ACCESS_TOKEN}@github.com/${MANIFEST_REPO}"
-                            // }
-
-                            // dir("${MANIFEST_DIR}") {  // GitHub 저장소 이름으로 디렉토리를 변경합니다.
-                            //     sh """
-                            //     sed -i 's|${DOCKERHUB_USERNAME}/${IMAGE_NAME}:.*|${DOCKERHUB_USERNAME}/${IMAGE_NAME}:${env.DOCKER_TAG}|' ${MANIFEST_DIR}/${MANIFEST_FILE}
-                            //     git config user.name "${env.GIT_AUTHOR_NAME}"
-                            //     git config user.email "${env.GIT_AUTHOR_EMAIL}"
-                            //     git add .
-                            //     git commit -m "Update image tag to ${env.DOCKER_TAG} from ${env.GIT_COMMIT}"
-                            //     git push origin ${env.GIT_BRANCH.replace('origin/', '')}  // GIT_BRANCH의 'origin/' 접두사를 제거
-                            //     """
-                            // }
                         }
-                        // withCredentials([usernamePassword(credentialsId: 'github-cridentials', passwordVariable: 'GIT_PASSWORD', usernameVariable: 'GIT_USERNAME')]) {
-                        //     sh "git clone https://${GIT_USERNAME}:${GIT_PASSWORD}@github.com/${MANIFEST_REPO}"
-                        //     sh """
-                        //     sed -i 's|${DOCKERHUB_USERNAME}/${IMAGE_NAME}:.*|${DOCKERHUB_USERNAME}/${IMAGE_NAME}:${env.DOCKER_TAG}|' ${MANIFEST_DIR}/${MANIFEST_FILE}
-                        //     """
-                        //     dir(MANIFEST_DIR) {
-                        //         sh """
-                        //         git config user.name "${env.GIT_AUTHOR_NAME}"
-                        //         git config user.email "${env.GIT_AUTHOR_EMAIL}"
-                        //         git add .
-                        //         git commit -m "Update image tag to ${env.DOCKER_TAG} from ${env.GIT_COMMIT}"
-                        //         git push origin ${env.GIT_BRANCH.replace('origin/', '')}  // GIT_BRANCH의 'origin/' 접두사를 제거
-                        //         """
-                        //     }
-                        //     // dir(MANIFEST_DIR) {
-                        //     //     sh """
-                        //     //     git config user.name "DW-K"
-                        //     //     git config user.email "pch145@naver.com"
-                        //     //     git add .
-                        //     //     git commit -m "Update image tag to ${env.DOCKER_TAG}"
-                        //     //     git push origin main
-                        //     //     """
-                        //     // }
-                        // }
                     }
                 }
             }
@@ -160,6 +117,10 @@ def call(Closure body) {
             }
         }
         post {
+            script {
+                    echo "An error occurred. Keeping the pod running for debugging..."
+                    sleep 3600 // Pod will be kept running for 1 hour
+            }
             failure {
                 script {
                     echo "An error occurred. Keeping the pod running for debugging..."
