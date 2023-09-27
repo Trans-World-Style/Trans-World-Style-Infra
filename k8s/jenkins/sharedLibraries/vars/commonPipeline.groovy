5384dafada1783a6685c8e2ddcbe3ee99ba7c071
DW-K <pch145@naver.com>
2023-09-27 15:04:37 +0900

    feat: argocd

--- k8s/jenkins/sharedLibraries/vars/commonPipeline.groovy
+++ k8s/jenkins/sharedLibraries/vars/commonPipeline.groovy
@@ -39,6 +39,9 @@ def call(Closure body) {
         environment {
             DOCKERHUB_USERNAME = "'${config.dockerhubUsername ?: 'dodo133'}'" // 주의: 추가적인 작은따옴표
             IMAGE_NAME = "'${config.imageName ?: 'tws-ai'}'" // 주의: 추가적인 작은따옴표
+            MANIFEST_REPO = config.manifestRepo ?: 'Trans-World-Style/Trans-World-Style-Infra.git'
+            MANIFEST_DIR = config.manifestDir ?: 'Trans-World-Style-Infra/k8s/product/ai/cpu'
+            MANIFEST_FILE = config.manifestFile ?: 'ai-deploy-cpu.yaml'
             GIT_COMMIT_SHORT = sh(script: 'echo $GIT_COMMIT | cut -c 1-12', returnStdout: true).trim()
         }
         stages {
@@ -67,8 +70,42 @@ def call(Closure body) {
                 steps {
                     container('kaniko') {
                         script {
+                            // buildAndPush(DOCKERHUB_USERNAME, IMAGE_NAME, env.DOCKER_TAG)
                             sh "ls /kaniko/.docker"
                             sh "echo '${DOCKERHUB_USERNAME}/${IMAGE_NAME}:${env.DOCKER_TAG}'"
+                        }
+                    }
+                }
+            }
+
+            stage('Delivery To Github Manifest') {
+                steps {
+                    container('kaniko') {
+                        script {
+                            withCredentials([usernamePassword(credentialsId: 'github-cridentials', passwordVariable: 'GIT_PASSWORD', usernameVariable: 'GIT_USERNAME')]) {
+                                sh "git clone https://${GIT_USERNAME}:${GIT_PASSWORD}@github.com/${MANIFEST_REPO}"
+                                sh """
+                                sed -i 's|${DOCKERHUB_USERNAME}/${IMAGE_NAME}:.*|${DOCKERHUB_USERNAME}/${IMAGE_NAME}:${env.DOCKER_TAG}|' ${MANIFEST_DIR}/${MANIFEST_FILE}
+                                """
+                                dir(MANIFEST_DIR) {
+                                    sh """
+                                    git config user.name "${env.GIT_AUTHOR_NAME}"
+                                    git config user.email "${env.GIT_AUTHOR_EMAIL}"
+                                    git add .
+                                    git commit -m "Update image tag to ${env.DOCKER_TAG} from ${env.GIT_COMMIT}"
+                                    git push origin ${env.GIT_BRANCH.replace('origin/', '')}  // GIT_BRANCH의 'origin/' 접두사를 제거
+                                    """
+                                }
+                                // dir(MANIFEST_DIR) {
+                                //     sh """
+                                //     git config user.name "DW-K"
+                                //     git config user.email "pch145@naver.com"
+                                //     git add .
+                                //     git commit -m "Update image tag to ${env.DOCKER_TAG}"
+                                //     git push origin main
+                                //     """
+                                // }
+                            }
                         }
                     }
                 }
