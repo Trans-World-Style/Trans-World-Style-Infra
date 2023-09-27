444f65e742aa1cb72673ab5e60457923df9d5d7a
DW-K <pch145@naver.com>
2023-09-27 13:58:54 +0900

    feat: argocd

--- k8s/jenkins/sharedLibraries/vars/commonPipeline.groovy
+++ k8s/jenkins/sharedLibraries/vars/commonPipeline.groovy
@@ -1,4 +1,9 @@
-def call(Map config = [:]) {
+def call(Closure body) {
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+    
     pipeline {
         agent {
             kubernetes {
