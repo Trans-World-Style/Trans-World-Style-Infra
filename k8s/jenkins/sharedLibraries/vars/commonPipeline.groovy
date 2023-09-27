40eab2f774587db1e2468951558524b3960b77d2
DW-K <pch145@naver.com>
2023-09-27 13:51:27 +0900

    feat: argocd

---
+++ k8s/jenkins/sharedLibraries/vars/commonPipeline.groovy
@@ -0,0 +1,59 @@
+def call(Map config = [:]) {
+    pipeline {
+        agent {
+            kubernetes {
+                yaml '''
+                    apiVersion: v1
+                    kind: Pod
+                    metadata:
+                      labels:
+                        role: kaniko
+                    spec:
+                      containers:
+                      - name: kaniko
+                        image: gcr.io/kaniko-project/executor:debug
+                        command:
+                        - /busybox/cat
+                        imagePullPolicy: Always
+                        tty: true
+                        volumeMounts:
+                        - name: jenkins-docker-cfg
+                          mountPath: /kaniko/.docker
+                      volumes:
+                      - name: jenkins-docker-cfg
+                        projected:
+                          sources:
+                          - secret:
+                              name: dockerhub-secret
+                              items:
+                                - key: config.json
+                                  path: config.json
+                    '''
+            }
+        }
+        environment {
+            DOCKERHUB_USERNAME = config.dockerhubUsername ?: 'dodo133'
+            IMAGE_NAME = config.imageName ?: 'tws-ai'
+            GIT_COMMIT_SHORT = sh(script: 'echo $GIT_COMMIT | cut -c 1-7', returnStdout: true).trim()
+        }
+        stages {
+            stage('extract docker tag') {
+                steps {
+                    script {
+                        env.DOCKER_TAG = extractDockerTag()
+                    }
+                }
+            }
+            stage('Build and Push') {
+                steps {
+                    container('kaniko') {
+                        script {
+                            sh "ls /kaniko/.docker"
+                            sh "echo '${DOCKERHUB_USERNAME}/${IMAGE_NAME}:${env.DOCKER_TAG}'"
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
