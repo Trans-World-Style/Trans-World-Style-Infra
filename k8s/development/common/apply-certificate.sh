80b1255e1bc9223a735df5de0fd5ce0e38d8639c
DW-K <pch145@naver.com>
2023-10-17 22:07:06 +0900

    add kubed

---
+++ k8s/development/common/apply-certificate.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+THIS_SCRIPT_PATH=$(cd "$(dirname "$0")" && pwd)
+cd "$THIS_SCRIPT_PATH"
+
+NAMESPACE="dev"
+
+# create the argocd namespace
+kubectl create ns "$NAMESPACE" || echo "namespace '$NAMESPACE' already exists"
+
+# apply certificate
+kubectl apply -f ./certificate.yaml
+
+# add annotation for kubed
+kubectl annotate secret dev-tw-style-duckdns-tls-secret kubed.appscode.com/sync="app=dev"
