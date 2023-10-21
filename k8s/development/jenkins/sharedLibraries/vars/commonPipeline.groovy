b64dfa8caf3bd94bf0bbecae3124ed5f0c6e5a49
DW-K <pch145@naver.com>
2023-10-21 19:10:59 +0900

    add: gateway cm

--- k8s/development/jenkins/sharedLibraries/vars/commonPipeline.groovy
+++ k8s/development/jenkins/sharedLibraries/vars/commonPipeline.groovy
@@ -6,8 +6,10 @@ def call(Closure body) {
 
     properties([
       parameters([
-        string(name: 'CONFIG_MAP_NAME', defaultValue: 'default-config', description: 'Name of the ConfigMap'),
-        string(name: 'CONFIG_MAP_MOUNT_PATH', defaultValue: '/config', description: 'Mount path for the ConfigMap in the container')
+        string(name: 'CONFIG_MAP_NAME', defaultValue: '', description: 'Name of the ConfigMap'),
+        string(name: 'CONFIG_MAP_NAMESPACE', defaultValue: '', description: 'Namespace of the ConfigMap'),
+        string(name: 'CONFIG_FILE_NAME', defaultValue: '', description: 'Name of the Config File from ConfigMap')
+        string(name: 'CONFIG_MAP_MOUNT_PATH', defaultValue: '', description: 'Mount path for the ConfigMap in the container')
       ])
     ])
 
@@ -15,93 +17,102 @@ def call(Closure body) {
         error("Mandatory config values are missing!")
     }
     // Check if ConfigMap should be used
-    def useConfigMap = !(params.CONFIG_MAP_NAME == 'default-config' && params.CONFIG_MAP_MOUNT_PATH == '/config')
+    def useConfigMap = !(params.CONFIG_MAP_NAME == '' || params.CONFIG_MAP_NAMESPACE == '' || params.CONFIG_MAP_MOUNT_PATH == '' || params.CONFIG_MAP_NAME == '')
 
-    // Dynamically construct Pod YAML
-    def podYaml = """
-        apiVersion: v1
-        kind: Pod
-        metadata:
-          labels:
-            jenkins: slave
-        spec:
-          containers:
-          - name: kaniko
-            image: gcr.io/kaniko-project/executor:debug
-            command:
-            - /busybox/cat
-            imagePullPolicy: Always
-            tty: true
-            volumeMounts:
-            - name: jenkins-docker-cfg
-              mountPath: /kaniko/.docker
-    """
-    
-    if (useConfigMap) {
-        podYaml += """
-            - name: application-cfg
-              mountPath: "${params.CONFIG_MAP_MOUNT_PATH}"
-        """
-    }
+    // // Dynamically construct Pod YAML
+    // def podYaml = """
+    //     apiVersion: v1
+    //     kind: Pod
+    //     metadata:
+    //       labels:
+    //         jenkins: slave
+    //     spec:
+    //       containers:
+    //       - name: kaniko
+    //         image: gcr.io/kaniko-project/executor:debug
+    //         command:
+    //         - /busybox/cat
+    //         imagePullPolicy: Always
+    //         tty: true
+    //         volumeMounts:
+    //         - name: jenkins-docker-cfg
+    //           mountPath: /kaniko/.docker
+    // """
 
-    podYaml += """
-          volumes:
-          - name: jenkins-docker-cfg
-            projected:
-              sources:
-              - secret:
-                  name: dockerhub-secret
-                  items:
-                    - key: config.json
-                      path: config.json
-    """
+    // if (useConfigMap) {
+    //     podYaml += """
+    //         - name: shared-volume
+    //           mountPath: "${params.CONFIG_MAP_MOUNT_PATH}"
+    //       initContainers:
+    //       - name: load-config
+    //         image: bitnami/kubectl:latest
+    //         command:
+    //         - sh
+    //         - -c
+    //         - |
+    //           kubectl get configmap ${params.CONFIG_MAP_NAME} -n prod -o jsonpath='{.data}' > /shared/configmap-data
+    //         volumeMounts:
+    //         - name: shared-volume
+    //           mountPath: /shared
+    //     """
+    // }
 
-    if (useConfigMap) {
-        podYaml += """
-          - name: application-cfg-volume
-            configMap:
-              name: "${params.CONFIG_MAP_NAME}"
-        """
-    }
+    // podYaml += """
+    //       volumes:
+    //       - name: jenkins-docker-cfg
+    //         projected:
+    //           sources:
+    //           - secret:
+    //               name: dockerhub-secret
+    //               items:
+    //                 - key: config.json
+    //                   path: config.json
+    // """
+
+    // if (useConfigMap) {
+    //     podYaml += """
+    //       - name: shared-volume
+    //         emptyDir: {}
+    //     """
+    // }
 
 // plugins/kaniko:1.8
     pipeline {
         agent {
             kubernetes {
-                yaml podYaml
-
-                // yaml '''
-                //     apiVersion: v1
-                //     kind: Pod
-                //     metadata:
-                //       labels:
-                //         jenkins: slave
-                //     spec:
-                //       containers:
-                //       - name: kaniko
-                //         image: gcr.io/kaniko-project/executor:debug
-                //         command:
-                //         - /busybox/cat
-                //         imagePullPolicy: Always
-                //         tty: true
-                //         volumeMounts:
-                //         - name: jenkins-docker-cfg
-                //           mountPath: /kaniko/.docker
-                //         - name: application-cfg
-                //           mountPath: "${params.CONFIG_MAP_MOUNT_PATH}"
-                //       volumes:
-                //       - name: jenkins-docker-cfg
-                //         projected:
-                //           sources:
-                //           - secret:
-                //               name: dockerhub-secret
-                //               items:
-                //                 - key: config.json
-                //                   path: config.json
-                //       - name: application-cfg-volume
-                //         configMap:
-                //           name: "${params.CONFIG_MAP_NAME}"
-                //     '''
+                // yaml podYaml
+                yaml '''
+                    apiVersion: v1
+                    kind: Pod
+                    metadata:
+                      labels:
+                        jenkins: slave
+                    spec:
+                      containers:
+                      - name: kaniko
+                        image: gcr.io/kaniko-project/executor:debug
+                        command:
+                        - /busybox/cat
+                        imagePullPolicy: Always
+                        tty: true
+                    '''
+                    // volumeMounts:
+                    //     - name: jenkins-docker-cfg
+                    //       mountPath: /kaniko/.docker
+                    //     - name: application-cfg
+                    //       mountPath: "${params.CONFIG_MAP_MOUNT_PATH}"
+                    //   volumes:
+                    //   - name: jenkins-docker-cfg
+                    //     projected:
+                    //       sources:
+                    //       - secret:
+                    //           name: dockerhub-secret
+                    //           items:
+                    //             - key: config.json
+                    //               path: config.json
+                    //   - name: application-cfg-volume
+                    //     configMap:
+                    //       name: "${params.CONFIG_MAP_NAME}"
             }
         }
         environment {
@@ -136,6 +147,20 @@ def call(Closure body) {
                 }
             }
 
+            stage('Load ConfigMap Data') {
+                when {
+                    expression { return useConfigMap }
+                }
+                steps {
+                    script {
+                        // kubectl을 사용하여 configMap 데이터를 불러와 workspace에 저장
+                        sh """
+                            kubectl get configmap ${params.CONFIG_MAP_NAME} -n ${params.CONFIG_MAP_NAMESPACE} -o jsonpath='{.data}' > ${params.CONFIG_FILE_NAME}
+                        """
+                    }
+                }
+            }
+
             stage('Build and Push') {
                 steps {
                     container('kaniko') {
@@ -143,10 +168,15 @@ def call(Closure body) {
                             withCredentials([usernamePassword(credentialsId: 'dockerhub-secret',
                                 usernameVariable: 'DOCKERHUB_ID', passwordVariable: 'DOCKERHUB_TOKEN')]) {
                             
-                                env.DOCKERHUB_USERNAME = DOCKERHUB_ID
-                                buildAndPush(env.DOCKERHUB_USERNAME, IMAGE_NAME, env.IMAGE_TAG)
+                                // env.DOCKERHUB_USERNAME = DOCKERHUB_ID
+                                // buildAndPush(env.DOCKERHUB_USERNAME, IMAGE_NAME, env.IMAGE_TAG)
                                 // sh "ls /kaniko/.docker"
                                 // sh "echo ${env.DOCKERHUB_USERNAME}/${IMAGE_NAME}:${env.IMAGE_TAG}"
+                                sh """
+                                export DOCKER_USERNAME=${DOCKERHUB_ID}
+                                export DOCKER_PASSWORD=${DOCKERHUB_TOKEN}
+                                /kaniko/executor --context `pwd` --destination ${imageFullName} --cache
+                                """
                             }
                         }
                     }
