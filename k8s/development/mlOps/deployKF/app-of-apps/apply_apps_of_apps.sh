c6ee8de35e194b88cc5a96d46603a635ec682d5f
DW-K <pch145@naver.com>
2023-10-23 19:40:02 +0900

    deployKF

--- k8s/development/mlOps/deployKF/app-of-apps/apply_apps_of_apps.sh
+++ k8s/development/mlOps/deployKF/app-of-apps/apply_apps_of_apps.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 THIS_SCRIPT_PATH=$(cd "$(dirname "$0")" && pwd)
 cd "$THIS_SCRIPT_PATH"
 
-ARGOCD_NAMESPACE="mlops-argocd"
+ARGOCD_NAMESPACE="argocd"
 
 # apply the argocd apps
 kubectl apply -f ./app-of-apps.yaml --namespace "$ARGOCD_NAMESPACE"
